3320b888bca8379a1b90ba9b3ee575040b7fb33c
Dominic Rozario <domnic85467@gmail.com>
2026-09-05 12:24:34 +0530

    fix services import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Template from './components/Template'
-import Services from './components/services'
+import Services from './components/Services'
 import Welcome from './components/Welcome'
 import Error from './components/Error'
 
